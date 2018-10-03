a2c3ffcd30308b1d3fe009751490517cc91f04e5
Martin <martinmoellerzbc@gmail.com>
2018-10-03 13:53:45 +0200

    added button to test connection to rest server

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import 'bootstrap/dist/css/bootstrap.css'
 
 
+const getZipCodes = (url) => {
+    fetch(url, { method: `GET` })
+        .then((response) => response.json())
+        .then((data) => {
+            let content = data.map(el => `<p>${el.zipCode}</p>`).join("");
+            document.getElementById("zipCodes").innerHTML = content;
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+}
+
+window.onload = () => {
+    document.getElementById("btnZip").addEventListener("click", () => {
+        getZipCodes("http://localhost:8084/CA2/api/CityInfo/zipCodeList");
+    });
+}
